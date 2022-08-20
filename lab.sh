0da6f7249eb54f0e531b9987758a807e22c78023
HananSalemAlqarni <hsh7al@gmail.com>
2022-08-20 11:03:54 +0300

    sh

--- lab.sh
+++ lab.sh
@@ -25,15 +25,23 @@ sudo usermod -aG docker $USER
 newgrp docker
 docker  --version
 
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+
 minikube start
+
 "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
 output->kubectl: OK
+
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
 kubectl version
+
 git clone https://github.com/HananSalemAlqarni/Kubernetes.git
 ls
 cd Kubernetes
