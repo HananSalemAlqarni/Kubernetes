0945bf8aeccf84a9a7c1b89cb89b760a7a11e410
HananSalemAlqarni <hsh7al@gmail.com>
2022-08-20 11:02:37 +0300

    sh

--- lab.sh
+++ lab.sh
@@ -1,13 +1,30 @@
+
+Install and setup Docker
+1- we should launch instance – ubuntu then connected to the terminal
+
 sudo apt update
 sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
 sudo apt update
 sudo apt install docker
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo docker  --version
+
+THIS COMMAND TO USE DOCKER WITH OUT SUDO
 sudo usermod -aG docker $USER
 newgrp docker
+docker  --version
+
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 minikube start
@@ -24,4 +41,6 @@ ls
 kubectl create -f pod-defination.yml
 kubectlget pods
 kubectl get pods
-kubectl get pods -o wide sudo apt update
+kubectl get pods -o wide
+kubectl describe pod my-first-pod
+
